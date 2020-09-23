281e38c92d3beba2f6c9b11f5c2c378a185da297
Piotr Kniaź <pio.kniaz@gmail.com>
2020-09-23 22:21:54 +0200

    build: autoprefixer postcss add

---
+++ packages/client/postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      browsers: ['> 1%', 'ie 11'],
+    },
+  },
+};
--- packages/client/webpack/webpack.production.js
+++ packages/client/webpack/webpack.production.js
@@ -19,7 +19,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
