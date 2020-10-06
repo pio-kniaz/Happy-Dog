521c746bec1245d28569c91f7830fdc30106c581
Piotr Kniaź <pio.kniaz@gmail.com>
2020-10-06 21:35:29 +0200

    chore: serve SPA by express add

--- packages/server/webpack/webpack.prod.js
+++ packages/server/webpack/webpack.prod.js
@@ -3,9 +3,11 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const SRC_PATH = path.resolve(__dirname, '../src');
-const BUILD_PATH = path.resolve(__dirname, '../../../build/server');
+const ROOT_PATH = path.resolve(__dirname, '../../server');
+const SRC_PATH = path.join(ROOT_PATH, '/src');
+const BUILD_PATH = path.resolve(__dirname, '../../../build');
 
 module.exports = {
   mode: 'production',
@@ -13,6 +15,10 @@ module.exports = {
     main: path.resolve(SRC_PATH, 'server.js'),
   },
   target: 'node',
+  node: {
+    // DUE TO Webpack by default mocking the __dirname variable to / in bundle
+    __dirname: false,
+  },
   externals: [nodeExternals()],
   output: {
     filename: 'app.js',
@@ -33,6 +39,14 @@ module.exports = {
     new CleanWebpackPlugin({
       verbose: true,
       cleanOnceBeforeBuildPatterns: ['app.js'],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(ROOT_PATH, 'package.json'),
+          to: path.resolve(BUILD_PATH),
+        },
+      ],
     }),
   ],
   performance: {
